f5548cb163f1754540ac6405dbeaaede949fc09a
Gareth Jones <gareth.jones@ackama.com>
2026-03-12 07:38:41 +1300

    ci: include new files in new directories for pr body

--- scripts/list_changed_advisories.py
+++ scripts/list_changed_advisories.py
@@ -7,7 +7,9 @@ Lists links to the advisories that have been changed according to git
 import re
 import subprocess
 
-output = subprocess.check_output(['git', 'status', '--porcelain'], text=True)
+output = subprocess.check_output(
+  ['git', 'status', '--untracked-files', '--porcelain'], text=True
+)
 
 for line in output.split('\n'):
   match = re.match(r'.. advisories/\w+/(DRUPAL-.*)', line)
