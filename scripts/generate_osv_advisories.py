2b6a31d7801bd8a1819c6b1e1b59ac1d4bc6c33f
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-03-19 06:51:18 +1300

    fix: don't generate advisories for Drupal distributions (#148)
    Distributions are not sourced through Composer so these advisories
    currently don't map to an actual packages and are getting flagged by the
    `osv-linter`.
    
    Some of these advisories do seem to name specific modules which might be
    available via Composer, so these could come back in future but that'll
    require having the specific module represented in a machine-readable way

--- scripts/generate_osv_advisories.py
+++ scripts/generate_osv_advisories.py
@@ -323,11 +323,7 @@ def get_credits_from_sa(
   return sorted(credit_list, key=lambda c: c['name'])
 
 
-def determine_composer_package_name(sa_advisory: drupal.Advisory) -> str:
-  project = typing.cast(
-    drupal.Project, fetch_drupal_node(sa_advisory['field_project']['id'])
-  )
-
+def determine_composer_package_name(project: drupal.Project) -> str:
   project_name = project['field_project_machine_name']
   if project_name == 'drupal':
     project_name = 'core'
@@ -408,7 +404,15 @@ def build_osv_advisory(
     print(' \\- ' + text_is.notice('skipping as we do not have any affected versions'))
     return None
 
-  composer_package_name = determine_composer_package_name(sa_advisory)
+  project = typing.cast(
+    drupal.Project, fetch_drupal_node(sa_advisory['field_project']['id'])
+  )
+
+  # distributions don't fit within the Packagist ecosystem as they're not packages
+  if project['type'] == 'project_distribution':
+    return None
+
+  composer_package_name = determine_composer_package_name(project)
 
   ecosystem = 'Packagist'
 
